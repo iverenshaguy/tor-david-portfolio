98a1d0584a552708ce124196d3fabc695ba47dbf
Iveren Shaguy <iverenshaguy@gmail.com>
2025-10-04 21:19:08 -0400

    setup ftp action

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import createMDX from "@next/mdx";
 
 const nextConfig: import("next").NextConfig = {
+  output: "standalone",
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
